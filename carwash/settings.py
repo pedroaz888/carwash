f3b65b4e4986779ff0fdc19f4a5764e69dd38ef8
PedroAz <pdegutty@gmail.com>
2023-04-19 17:15:58 -0300

    01

--- carwash/settings.py
+++ carwash/settings.py
@@ -147,11 +147,8 @@ STATIC_ROOT = os.path.join('static') """
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "templates/static/*")
+    os.path.join(BASE_DIR, "templates/static/")
 ]
-
-
-
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "/media/"
