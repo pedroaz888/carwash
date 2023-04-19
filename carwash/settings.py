40bc9410bcc137907c54363722f2d413c08298bd
PedroAz <pdegutty@gmail.com>
2023-04-19 11:13:36 -0300

    deploying

--- carwash/settings.py
+++ carwash/settings.py
@@ -139,15 +139,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+""" STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static/'),)
+STATIC_ROOT = os.path.join('static') """
+
+
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
-STATIC_ROOT = os.path.join('static')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "templates/static/")
+]
+
+
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "/media/"
-
-
-
 
 
 
